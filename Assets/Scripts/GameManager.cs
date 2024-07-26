e3774bf7e99bf4d6d6a776e1367c4340419aa73e
Sack Valdez <rauldaxo@gmail.com>
2024-07-25 23:07:15 -0600

    Coroutine to spawn objects
    With a while loop, we spawn the prefabs list with a random index int

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private float spawnRate = 1.0f;
     public List<GameObject> targets;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Coroutine to spawn targets
+    IEnumerator SpawnTarget()
+    {
+        // Temporary loop to spawn targets
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            // Randomly select a target from the list and instantiate it
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 }
