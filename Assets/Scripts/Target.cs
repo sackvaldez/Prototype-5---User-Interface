9dfca5476a077d61fd11e2ad59613d7a3f8c298d
Sack Valdez <rauldaxo@gmail.com>
2024-07-28 22:57:08 -0600

    First TextMeshPro added
    It will act as a Score UI

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -8,7 +8,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
 
     private Rigidbody targetRb;
     // Start is called before the first frame update
